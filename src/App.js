09891173c6a56c599a90d072d9c44854459060fc
Shivamraj27 <sh25vm@gmail.com>
2021-12-16 22:46:07 +0530

    final react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
-function App() {
+import React, { useState } from "react";
+
+export default function App() {
+  const [height, setHeight] = useState();
+  const [mass, setMass] = useState();
+  const [bmi, setBmi] = useState();
+
+ 
+    
+  const calculate = (e) => {
+    e.preventDefault();
+    const formValid = +height > 0 && +mass > 0;
+    if (!formValid) {
+      return;
+    }
+    const bmi = +mass / (+height/100) ** 2;
+    setBmi(bmi);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div>
+      <Header />
+      
+    </div><div className="App">
+        <form onSubmit={calculate}>
+          <div>
+            <label>Height (in cm)</label>
+            <input value={height} onChange={(e) => setHeight(e.target.value)} />
+          </div>
+
+          <div>
+            <label>Weight (in kg)</label>
+            <input value={mass} onChange={(e) => setMass(e.target.value)} />
+          </div>
+
+          <button type="button" class="btn btn-success" type="submit">calculate</button>
+          
+          <button type="reset" class="btn btn-warning"  onClick={() => setHeight() & setMass() & setBmi()}>Reset</button>
+        </form>
+        
+
+        <div className="container" >
+      <div className="card">
+        <div className="card-content">
+          <span className="card-title" data-test="bmi">
+            BMI: {bmi}
+          </span>
+          <div className="card-data">
+            <span data-test="weight">Weight: {mass} kg</span>
+            <br/>         
+               <span data-test="height">Height: {height} cm</span>
+           
+          </div>
+
+         
+        </div>
+      </div>
     </div>
+
+        
+      </div></>
   );
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h4 className='my-3'>My BMI Tracker</h4>
+        </div>
+    )
+}
